d5836666f25dfde65595b72fda3e83eb9fbb6eaf
michel <michelc@gmail.com>
2010-03-13 13:54:10 +0100

    Création de quelques contacts de démonstration

--- MongoContact/Controllers/HomeController.cs
+++ MongoContact/Controllers/HomeController.cs
@@ -18,6 +18,37 @@ namespace MongoContact.Controllers
 
         public ActionResult Index()
         {
+            // Ajout de quelques données d'exemple
+            var count = _session.Contacts.Count();
+            if (count == 0)
+            {
+                _session.Add(new Contact { FirstName = "Scott",
+                                            LastName = "Guthrie", 
+                                            Phone = "555-444-5556", 
+                                            Email = "scott@somewhere.com" });
+                _session.Add(new Contact { FirstName = "Phil",
+                                            LastName = "Haack", 
+                                            Phone = "206-777-5555", 
+                                            Email = "phil@somewhere.com" });
+                _session.Add(new Contact { FirstName = "Stephen", 
+                                            LastName = "Walther", 
+                                            Phone = "206-555-8988", 
+                                            Email = "steve@somewhere.com" });
+                _session.Add(new Contact { FirstName = "Eilon", 
+                                            LastName = "Lipton", 
+                                            Phone = "415-777-5555", 
+                                            Email = "eilon@somewhere.com" });
+                _session.Add(new Contact { FirstName = "Scott", 
+                                            LastName = "Hanselman", 
+                                            Phone = "555-444-5555", 
+                                            Email = "scottha@somewhere.com" });
+                _session.Add(new Contact { FirstName = "Rob", 
+                                            LastName = "Conery", 
+                                            Phone = "333-899-9999", 
+                                            Email = "rob@somewhere.com" });
+            }
+
+            // Renvoie la vue pour afficher la liste des contacts
             return View(_session.Contacts.ToList());
         }
 
